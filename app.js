fc62f16c915a5e741b7ec8861d140b040587c99f
so2radar <maks.vanyushin0907@gmail.com>
2026-06-20 00:19:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -77,13 +77,21 @@ const peer = new Peer(undefined, {
     path: '/peerjs',
     config: {
         'iceServers': [
-            { urls: 'stun:stun.yandex.ru:3478' },
             { urls: 'stun:stun.l.google.com:19302' },
-            { urls: 'stun:global.stun.twilio.com:3478' }
+            { urls: 'stun:stun.yandex.ru:3478' },
+            {
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            },
+            {
+                urls: 'turn:openrelay.metered.ca:443',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            }
         ]
     }
 });
-
 
 let currentConnection = null;
 
