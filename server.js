cc5db40f4c3c9bb5d0ed0e4fe5d3532381ce42d5
so2radar <maks.vanyushin0907@gmail.com>
2026-06-19 23:42:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+
+// Просто заглушка, чтобы проверить, что сервер работает
+app.get('/', (req, res) => res.send('Сервер радара активен!'));
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Личный сервер запущен');
+});
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/'
+});
+
+app.use('/peerjs', peerServer);
 
